3e1ba44d235465a0954a7f6ef4b8b8c4c15f4170
cendiastian <70523613+cendiastian@users.noreply.github.com>
2021-09-19 21:47:12 +0700

    fitur" user complete

--- routes/route.go
+++ routes/route.go
@@ -11,8 +11,10 @@ func NewRoute() *echo.Echo {
 	eTest.GET("users", getUser)
 	eTest.POST("users/register", userRegist)
 	eTest.GET("users/:userId", userDetail)
-	// eTest.POST("users/login", userLogin)
-	// eTest.PUT("/users/:id", updateUser)
-	// eTest.DELETE("/users/:id", deleteUser)
+	eTest.POST("users/login", userLogin)
+	eTest.PUT("users/update", updateUser)
+	eTest.DELETE("users/delete", deleteUser)
+	// eTest1 := e.Group("movies/")
+	// eTest1.GET("movies", getMovie)
 	return e
 }
